2228200f3bd2cb5c6f88fe6f77d5f963805f89ed
congson237 <congsonvetyp@gmail.com>
2024-08-31 13:37:28 +0700

    Bai 02A

--- Baitap02A/Program.cs
+++ Baitap02A/Program.cs
@@ -21,7 +21,17 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
+   name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+app.MapControllerRoute(
+    name: "trang-chu",
+    pattern: "trang-chu/{action=Index}/{id?}",
+    defaults: new { controller = "Home", action = "Index" });
+
+app.MapControllerRoute(
+    name: "the-loai",
+    pattern: "the-loai/{action=Index}/{id?}",
+    defaults: new { controller = "TheLoai", action = "Index" });
 app.Run();
