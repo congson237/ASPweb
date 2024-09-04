60e5a9c26b38ba287820647ad08070368102c1c4
congson237 <congsonvetyp@gmail.com>
2024-09-04 15:11:21 +0700

    Baitap05

---
+++ BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 2";
+            ViewData["Nam"] = "Năm 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var TheLoai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Thám"
+            };
+            return View(TheLoai);
+        }
+    }
+}
