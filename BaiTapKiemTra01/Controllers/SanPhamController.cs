d0edb1d29d66624f785cd289a7bee234189f72f2
congson237 <congsonvetyp@gmail.com>
2024-09-14 15:17:04 +0700

    BaiTapKiemTra

---
+++ BaiTapKiemTra01/Controllers/SanPhamController.cs
@@ -0,0 +1,22 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BaiTap2()
+        {
+            var sanpham = new SanPhamViewModel();
+            { 
+            
+            };
+            return View(sanpham);
+        }
+    }
+
+}
---
+++ BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,29 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (model.TenTaiKhoan != null)
+            {
+               
+                string thongTin = $"Tên đăng nhập:{model.TenTaiKhoan}" + " "+
+                                  $"Mật khẩu:{model.MatKhau}" + "  " +
+                                  $"Họ tên:{model.HoTen}" + "  " +
+                                  $"Tuổi:{model.Tuoi}";
+
+            
+                return Content(thongTin);
+            }
+
+            return View();
+        }
+    }
+}
